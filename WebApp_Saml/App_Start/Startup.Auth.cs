652084d3ba840cbaa81f304df86eb57142fae6ce
soumik06061984az96 <soumik-06061984-az96@outlook.com>
2022-01-06 22:35:16 -0700

    Code Updates

---
+++ WebApp_Saml/App_Start/Startup.Auth.cs
@@ -0,0 +1,32 @@
+﻿using System.Configuration;
+using Owin;
+using Microsoft.Owin.Host.SystemWeb;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.WsFederation;
+using Microsoft.IdentityModel.Logging;
+
+namespace WebApp_Saml
+{
+    public partial class Startup
+    {
+        private static string _realm = ConfigurationManager.AppSettings["ida:Wtrealm"];
+        private static string _adfsMetadata = ConfigurationManager.AppSettings["ida:ADFSMetadata"];
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            IdentityModelEventSource.ShowPII = true;
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            app.UseWsFederationAuthentication(
+                new WsFederationAuthenticationOptions
+                {
+                    Wtrealm = _realm,
+                    MetadataAddress = _adfsMetadata,
+                    Wreply = "https://localhost:44311/"
+                });
+        }
+    }
+}
---
+++ WebApp_Saml/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(WebApp_Saml.Startup))]
+
+namespace WebApp_Saml
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
